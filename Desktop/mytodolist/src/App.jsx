be65f91f203c1ed0fe068382f8cc836a045d61da
hghyhghy <subhamsarkar.aeie2021@nsec.ac.in>
2024-02-27 12:37:07 +0530

    First commit

---
+++ Desktop/mytodolist/src/App.jsx
@@ -0,0 +1,35 @@
+
+
+
+import { useState } from 'react'
+import { FcTodoList } from "react-icons/fc";
+
+import './index.css'
+import Navbar from './Components/Navbar'
+import Mytodo from './Components/Mytodo';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar/>
+    <div className=" mx-auto p-[2%] bg-zinc-900 w-full h-full mt-0">
+      
+      <div className="w-1/2 p-5 rounded-xl bg-violet-200 ml-[24%] flex gap-4 items-center mytodo"> 
+      
+       
+      <h1 className=' ml-[41%] text-2xl text-zinc-900'>My to do list </h1>
+      <FcTodoList  className="text-xl bg-violet-700 symbol" />
+
+
+
+      </div>
+      <Mytodo/>
+
+    </div>
+    </>
+  )
+}
+
+export default App
